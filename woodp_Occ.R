3d3c5488b5bfa767d17dc57c90cdcc117f2ddb79
danlinden <danlinden@gmail.com>
2017-06-02 17:12:04 -0400

    change to untransformed data; predict and plot model

--- woodp_Occ.R
+++ woodp_Occ.R
@@ -9,24 +9,49 @@ woodpecker <- read.csv(text = x)
 head(woodpecker)
 
 woodp.y <- woodpecker[,c("y.1","y.2","y.3")]
-
 woodp.sitecovs <- woodpecker[,c("snags"),drop=F]
-
 woodp.obsCovs <- woodpecker[,c("date.1","date.2","date.3")]
 
+# create unmarked frame with data
 umf <- unmarkedFrameOccu(y = woodp.y,
                          siteCovs = woodp.sitecovs,
                          obsCovs = list(date=woodp.obsCovs))
 umf
 summary(umf)
 
+# fit a collection of models
 modList <- list(
   #~p ~psi 
-  "p(.)psi(.)"          = occu(~1    ~1,     umf),  # fit a model
-  "p(date)psi(.)"       = occu(~date ~1,     umf),  # fit a model
-  "p(.)psi(snags)"      = occu(~1    ~snags, umf),  # fit a model
-  "p(date)psi(snags)"   = occu(~date ~snags, umf)  # fit a model
+  "p(.)psi(.)"          = occu(~1           ~1,     umf),  
+  "p(date)psi(.)"       = occu(~scale(date) ~1,     umf),  
+  "p(.)psi(snags)"      = occu(~1           ~log(snags), umf),  
+  "p(date)psi(snags)"   = occu(~scale(date) ~log(snags), umf)
 )
 # AIC model selection
 aictab(modList)
 
+#------------------
+# model predictions
+#------------------
+
+# predict/plot date effect on detection
+date.dat <- data.frame(date=150:190)
+date.fit <- predict(modList[[4]],type="det",newdata=date.dat,appendData=T)
+
+with(date.fit, {
+  plot(date, Predicted, type="l",xlim=c(150,190),ylim=c(0,1),las=1,bty="n",lwd=2,
+       xlab="Ordinal date", ylab="Detection probability")
+  lines(date, Predicted+1.96*SE, lty=2)
+  lines(date, Predicted-1.96*SE, lty=2)
+})
+
+# predict/plot snag density effect on occupancy
+snags.dat <- data.frame(snags=seq(0.25,6,by=0.25))
+snags.fit <- predict(modList[[4]],type="state",newdata=snags.dat,appendData=T)
+
+with(snags.fit, {
+  plot(snags, Predicted, type="l",xlim=c(0,6),ylim=c(0,1),las=1,bty="n",lwd=2,
+       xlab="Snag density (#/ha)", ylab="Nest occupancy probability")
+  lines(snags, Predicted+1.96*SE, lty=2)
+  lines(snags, Predicted-1.96*SE, lty=2)
+})
